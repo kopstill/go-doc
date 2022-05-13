a4d7d8f3eb2df9c7e6ffc7011e12b2d5e8fe0294
kopever <kop.still@gmail.com>
2022-05-13 22:35:20 +0800

    gin: write a handler to return a specific item

--- web-service-gin/main.go
+++ web-service-gin/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -24,7 +25,8 @@ var albums = []album{
 func main() {
 	router := gin.Default()
 	router.GET("/albums", getAlbums)
-	router.POST("/albums", postAlbums)
+	router.GET("/album/:id", getAlbumByID)
+	router.POST("/album", postAlbums)
 
 	router.Run("localhost:8080")
 }
@@ -41,10 +43,27 @@ func postAlbums(c *gin.Context) {
 	// Call BindJSON to bind the received JSON to
 	// newAlbum.
 	if err := c.BindJSON(&newAlbum); err != nil {
-		return
+		log.Fatal(err)
+		//return
 	}
 
 	// Add the new album to the slice.
 	albums = append(albums, newAlbum)
 	c.IndentedJSON(http.StatusCreated, newAlbum)
+}
+
+// getAlbumByID locates the album whose ID value matches the id
+// parameter sent by the client, then returns that album as a response.
+func getAlbumByID(c *gin.Context) {
+	id := c.Param("id")
+
+	// Loop over the list of albums, looking for
+	// an album whose ID value matches the parameter.
+	for _, a := range albums {
+		if a.ID == id {
+			c.IndentedJSON(http.StatusOK, a)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
 }
